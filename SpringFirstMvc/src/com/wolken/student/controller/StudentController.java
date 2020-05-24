a2389aecc250a28c0e84a6c1834be00428204072
adithya <adithya@wolkensoftware.com>
2020-05-24 15:27:06 +0530

    third commit

--- SpringFirstMvc/src/com/wolken/student/controller/StudentController.java
+++ SpringFirstMvc/src/com/wolken/student/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.wolken.student.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,5 +18,13 @@ public class StudentController {
 	@RequestMapping(value = "/pinNumber", method = RequestMethod.GET)
 	public int getPinCode() {
 		return 571111;
+	}
+
+	@RequestMapping(value = "/display", method = RequestMethod.GET)
+	public String homePage(Model model) {
+		model.addAttribute("studFName", "Adithya");
+		model.addAttribute("studLName", 'S');
+		System.out.println("\"display\" is a void method");
+		return "home";
 	}
 }
