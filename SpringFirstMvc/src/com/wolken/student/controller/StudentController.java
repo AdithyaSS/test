7eabcb58e828fa83bb87a69bf8383623300ce0af
adithya <adithya@wolkensoftware.com>
2020-05-24 15:45:56 +0530

    fourth commit

--- SpringFirstMvc/src/com/wolken/student/controller/StudentController.java
+++ SpringFirstMvc/src/com/wolken/student/controller/StudentController.java
@@ -1,22 +1,23 @@
 package com.wolken.student.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class StudentController {
 	@ResponseBody
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String sayHello() {
-		return "Hello, good afternoon...!";
+		return "Hello, good afternoon...! Adithya";
 	}
 
 	@ResponseBody
 	@RequestMapping(value = "/pinNumber", method = RequestMethod.GET)
-	public int getPinCode() {
+	public Integer getPinCode() {
 		return 571111;
 	}
 
@@ -26,5 +27,10 @@ public class StudentController {
 		model.addAttribute("studLName", 'S');
 		System.out.println("\"display\" is a void method");
 		return "home";
+	}
+
+	@RequestMapping(value = "/myView", method = RequestMethod.GET)
+	public ModelAndView getModelAndView() {
+		return new ModelAndView("myPage");
 	}
 }
